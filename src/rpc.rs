ce48edd16cccd4613477700698e129fc28813bc0
valued mammal <valuedmammal@protonmail.com>
2026-08-17 22:38:59 -0400

    refactor(rpc): Manually implement Rpc enum
    Removed impl_rpc_methods macro, which previously
    allocated a string at runtime (due to `to_lowercase`)
    every time to_string was called.
    Here the RPC method names are defined statically.

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,44 +1,85 @@
 //! [`Rpc`] methods
 
-macro_rules! impl_rpc_methods {
-    ( $($name:ident,)+ ) => {
-        /// RPCs
-        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-        #[allow(missing_docs)]
-        pub enum Rpc {
-            $(
-                $name,
-            )+
-        }
-
-        impl core::fmt::Display for Rpc {
-            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-                let s = match self {
-                    $(
-                        Self::$name => stringify!($name).to_lowercase(),
-                    )+
-                };
-
-                f.write_str(&s)
-            }
-        }
-    }
-}
+use core::fmt::{self, Display};
 
 // RPC methods go here. These names MUST match the name of the RPC method (when converted to lowercase).
 // See <https://bitcoincore.org/en/doc/> for details.
-impl_rpc_methods!(
+/// RPCs
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Rpc {
+    /// `getbestblockhash`
     GetBestBlockHash,
+    /// `getblockchaininfo`
     GetBlockchainInfo,
+    /// `getblockhash`
     GetBlockHash,
+    /// `getblockcount`
     GetBlockCount,
+    /// `getblock`
     GetBlock,
+    /// `getblockheader`
     GetBlockHeader,
+    /// `getblockfilter`
     GetBlockFilter,
+    /// `getdescriptorinfo`
     GetDescriptorInfo,
+    /// `getrawmempool`
     GetRawMempool,
+    /// `sendtoaddress`
     SendToAddress,
+    /// `getrawtransaction`
     GetRawTransaction,
+    /// `importdescriptors`
     ImportDescriptors,
+    /// `estimatesmartfee`
     EstimateSmartFee,
-);
+}
+
+impl Display for Rpc {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match self {
+            Self::GetBestBlockHash => "getbestblockhash",
+            Self::GetBlockchainInfo => "getblockchaininfo",
+            Self::GetBlockHash => "getblockhash",
+            Self::GetBlockCount => "getblockcount",
+            Self::GetBlock => "getblock",
+            Self::GetBlockHeader => "getblockheader",
+            Self::GetBlockFilter => "getblockfilter",
+            Self::GetDescriptorInfo => "getdescriptorinfo",
+            Self::GetRawMempool => "getrawmempool",
+            Self::SendToAddress => "sendtoaddress",
+            Self::GetRawTransaction => "getrawtransaction",
+            Self::ImportDescriptors => "importdescriptors",
+            Self::EstimateSmartFee => "estimatesmartfee",
+        };
+        write!(f, "{s}")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Rpc;
+    use alloc::string::ToString;
+
+    #[test]
+    fn test_rpc_names() {
+        for (rpc, name) in [
+            (Rpc::GetBestBlockHash, "getbestblockhash"),
+            (Rpc::GetBlockchainInfo, "getblockchaininfo"),
+            (Rpc::GetBlockHash, "getblockhash"),
+            (Rpc::GetBlockCount, "getblockcount"),
+            (Rpc::GetBlock, "getblock"),
+            (Rpc::GetBlockHeader, "getblockheader"),
+            (Rpc::GetBlockFilter, "getblockfilter"),
+            (Rpc::GetDescriptorInfo, "getdescriptorinfo"),
+            (Rpc::GetRawMempool, "getrawmempool"),
+            (Rpc::SendToAddress, "sendtoaddress"),
+            (Rpc::GetRawTransaction, "getrawtransaction"),
+            (Rpc::ImportDescriptors, "importdescriptors"),
+            (Rpc::EstimateSmartFee, "estimatesmartfee"),
+        ] {
+            let result = rpc.to_string();
+            assert_eq!(result, name, "expected Rpc name {name}, got {result}");
+        }
+    }
+}
